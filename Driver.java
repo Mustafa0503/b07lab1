3a0ffb85b01bc9afc17abe3beb65bb05ced8f322
Mustafa0503 <a1533440507@gmail.com>
2022-10-04 11:18:32 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,150 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 public class Driver { 
-	 public static void main(String [] args) { 
-	  Polynomial p = new Polynomial(); 
-	  System.out.println(p.evaluate(3)); 
-	  double [] c1 = {6,0,0,5}; 
-	  Polynomial p1 = new Polynomial(c1); 
-	  double [] c2 = {0,-2,0,0,-9}; 
-	  Polynomial p2 = new Polynomial(c2); 
-	  Polynomial s = p1.add(p2); 
-	  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-	  if(s.hasRoot(1)) 
-	   System.out.println("1 is a root of s"); 
-	  else 
-	   System.out.println("1 is not a root of s"); 
-	 } 
-	} 
+	 public static void main(String [] args) throws IOException { 
+		 String a = "-x";
+		 String[] sp = a.split("x");
+		 for(int i=0;i<sp.length;i++)
+		 {
+			// System.out.println(sp[i]);
+		 }
+		 
+	  //Polynomial p = new Polynomial(); 
+	  //System.out.println(p.evaluate(3)); 
+		 
+	 /* double [] c1 = {6,-7}; 
+	  int [] cc= {0,3};
+	 
+	  double [] c2 = {-2,9,7,-9};
+	  int [] cc1= {1,2,3,4};
+	  Polynomial p1 = new Polynomial(c1,cc); 
+	 // System.out.println("asd");
+	   
+	  Polynomial p2 = new Polynomial(c2,cc1); 
+	  
+	  Polynomial s = p1.add(p2);
+	  for(int i=0;i<s.a.length;i++)
+	  {
+		  System.out.println(s.a[i]);
+		  System.out.println(s.ex[i]);
+	  }
+	  System.out.println("//////////////////////////////////////////////////");
+	  Polynomial mul = p1.multiply(p2);
+	  for(int i=0; i<mul.a.length;i++)
+	  {
+		  System.out.println(mul.a[i]);
+		  System.out.println(mul.ex[i]);
+	  }
+	  //System.out.println("s(0.1) = " + s.evaluate(0.1)); 
+//	  if(s.hasRoot(1)) 
+//	   System.out.println("1 is a root of s"); 
+//	  else 
+//	   System.out.println("1 is not a root of s"); 
+	 } */
+		 	/*double c1[]= {1,2,1}, c2[] = {1,-5};
+	        int e1[]= {2,1,0}, e2[] = {1,0};
+
+	        double c3[] = {6,2,4,5};
+	        int e3[] = {3,2,1,4};
+
+	        double c4[] = {1,2,3};
+	        int e4[] = {5,3,1};
+
+	        Polynomial p1=new Polynomial(c1, e1);
+	        Polynomial p2=new Polynomial(c2, e2);
+
+
+	        Polynomial p3=p1.add(p2);
+
+	        Polynomial p4=new Polynomial(c3, e3);
+	        Polynomial p5=new Polynomial(c4, e4);
+
+	        Polynomial p6=p4.add(p5);
+
+	        //System.out.println(p3.evaluate(0));
+	        for(int i=0; i<p6.a.length; i++) {
+	
+	            System.out.println(p6.a[i] + " ");
+	            
+	            System.out.println(p6.ex[i] + " ");
+	        }
+	//
+	        System.out.println();
+
+//	        for(int i=0; i<p6.coefficients.length; i++) {
+	//
+//	            System.out.print(p6.exponents[i] + " ");
+//	        }
+	//
+	        Polynomial p7=p4.multiply(p5);
+
+//
+//	        for(int i=0; i<p7.a.length; i++) {
+//
+//	            System.out.print(p7.a[i] + " ");
+//	        }
+//	        System.out.println();
+//
+//	        for(int i=0; i<p7.ex.length; i++) {
+//
+//	            System.out.print(p7.ex[i] + " ");
+//	        }
+	 }
+}*/
+	
+
+
+	//public static void main(String[] args) {
+		 double c1[]= {15,17,-270}, c2[] = {1,-10};
+	        int e1[]= {3,100,4}, e2[] = {3,100};
+
+		
+		Polynomial p1=new Polynomial(c1, e1);
+		Polynomial p2=new Polynomial(c2, e2);
+		
+		
+		Polynomial p3=p1.multiply(p2);
+		for(int i=0; i<p3.a.length; i++) {
+			
+			System.out.println(p3.a[i]);	
+			System.out.println(p3.ex[i]);	
+		}
+		System.out.println("sad");
+		/*Polynomial p4=new Polynomial(c3, e3);
+		Polynomial p5=new Polynomial(c4, e4);
+		
+		Polynomial p6=p4.multiply(p5);
+		
+		//System.out.println(p3.evaluate(0));
+		for(int i=0; i<p6.a.length; i++) {
+			
+			System.out.print(p6.a[i] + " ");		
+		}
+		
+		System.out.println();
+		
+		for(int i=0; i<p6.a.length; i++) {
+			
+			System.out.print(p6.ex[i] + " ");			
+		}*/
+		
+		File file = new File("C:\\Users\\musta\\Desktop\\07\\b07Lec02\\lab2.txt");
+		Polynomial f = new Polynomial(file);
+		System.out.println("*******************");
+		for(int i=0; i<f.a.length;i++)
+		{
+			System.out.println(f.a[i]+"\t"+f.ex[i]);
+			//System.out.println(f.ex[i]);
+		}
+		f.saveToFile("do.txt");
+		double c4[] = {-1,1,-1,1};
+		int e4[] = {1,2,4,0};
+		Polynomial n = new Polynomial(c4,e4);
+		n.saveToFile("test.txt");
+	}
+
+}
--- Polynomial.java
+++ Polynomial.java
@@ -1,10 +1,20 @@
+package b07Lec02;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Polynomial {
 	double[] a;
+	int[] ex;
 
 	public Polynomial(){
 		a = new double[1];
+		ex= new int[1];
 	}
-	public Polynomial(double[] b)
+	public Polynomial(double[] b, int[]d)
 	{
 		int n = 0;
 		a = new double[b.length];
@@ -13,40 +23,188 @@ public class Polynomial {
 			a[n]=b[n];
 			n++;
 		}
+		n=0;
+		ex = new int[d.length];
+		while(n<d.length)
+		{
+			ex[n]=d[n];
+			n++;
+		}
 	}
 	public Polynomial add(Polynomial c)
 	{
 		Polynomial n;
+		int count=0;
+		int countt=0;
+		double []n_cof;
+		int []n_exp;
+		int l =0;
 		if(c.a.length>=a.length)
 		{
-			n=new Polynomial(c.a);
-			int i=0;
-			while(i<a.length)
+			n=new Polynomial(c.a, c.ex);
+			for(int i=0;i<ex.length;i++)
 			{
-				n.a[i]=c.a[i]+a[i];
-				i++;
+				for(int j=0;j<c.ex.length;j++)
+				{
+					if(ex[i]==c.ex[j])
+					{
+						count++;
+					}
+				}
 			}
+			int n_len = ex.length-count;
+			n_cof = new double [n_len];
+			n_exp = new int [n_len];
+			for(int i=0;i<ex.length;i++)
+			{
+				for(int j=0;j<c.ex.length;j++)
+				{
+					if(ex[i]==c.ex[j])
+					{
+						n.a[j]=a[i]+c.a[j];
+					}
+					else
+					{
+						countt++;
+					}
+				}
+				if(countt==c.ex.length)
+				{
+					n_cof[l] = a[i];
+					n_exp[l] = ex[i];
+					l++;
+				}
+				countt=0;
+			}
+			double[] f_cof = new double[n_cof.length+c.a.length];
+			int q=0;
+			int [] f_exp = new int[n_exp.length+c.ex.length];
+			for(int i=0; i<c.a.length;i++)
+			{
+				
+				//if(n.a[i]!=0.0)
+				{
+					f_cof[q]=n.a[i];
+					f_exp[q] = n.ex[i];
+					q++;
+				}
+			}
+			for(int i=0; i<n_cof.length;i++)
+			{
+				//if(n_cof[i]!=0.0)
+				{
+					f_cof[q]=n_cof[i];
+					f_exp[q]=n_exp[i];
+					q++;
+				}
+			}
+			int zs = 0;
+			for(int i=0;i<f_cof.length;i++)
+			{
+				if(f_cof[i]==0)
+				{
+					zs++;
+				}
+			}
+			double[] cof = new double [f_cof.length-zs];
+			int [] exp = new int [f_cof.length-zs];
+			int w=0;
+			for(int i=0;i<f_cof.length;i++)
+			{
+				if(f_cof[i]!=0.0)
+				{
+					cof[w]=f_cof[i];
+					exp[w]=f_exp[i];
+					w++;
+				}
+			}
+			n=new Polynomial(cof, exp);
 			return n;
 		}
 		else
 		{
-			int i = 0;
-			n = new Polynomial(a);
-			while(i<c.a.length)
+			n=new Polynomial(a, ex);
+			for(int i=0;i<c.ex.length;i++)
 			{
-				n.a[i]= c.a[i]+a[i];
-				i++;
+				for(int j=0;j<ex.length;j++)
+				{
+					if(c.ex[i]==ex[j])
+					{
+						count++;
+					}
+				}
 			}
+			int n_len = c.ex.length-count;
+			n_cof = new double [n_len];
+			n_exp = new int [n_len];
+			for(int i=0;i<c.ex.length;i++)
+			{
+				for(int j=0;j<ex.length;j++)
+				{
+					if(c.ex[i]==ex[j])
+					{
+						n.a[j]=c.a[i]+a[j];
+					}
+					else
+					{
+						countt++;
+					}
+				}
+				if(countt==ex.length)
+				{
+					n_cof[l] = c.a[i];
+					n_exp[l] = c.ex[i];
+					l++;
+				}
+				countt=0;
+			}
+			//for(int i=0; )
+			double[] f_cof = new double[n_cof.length+a.length];
+			int q=0;
+			int [] f_exp = new int[n_exp.length+ex.length];
+			
+			for(int i=0; i<a.length;i++)
+			{
+				f_cof[q]=n.a[i];
+				f_exp[q] = n.ex[i];
+				q++;
+			}
+			for(int i=0; i<n_cof.length;i++)
+			{
+				f_cof[q]=n_cof[i];
+				f_exp[q]=n_exp[i];
+				q++;
+			}
+			int zs = 0;
+			for(int i=0;i<f_cof.length;i++)
+			{
+				if(f_cof[i]==0)
+				{
+					zs++;
+				}
+			}
+			double[] cof = new double [f_cof.length-zs];
+			int [] exp = new int [f_cof.length-zs];
+			int w=0;
+			for(int i=0;i<f_cof.length;i++)
+			{
+				if(f_cof[i]!=0.0)
+				{
+					cof[w]=f_cof[i];
+					exp[w]=f_exp[i];
+					w++;
+				}
+			}
+			n=new Polynomial(cof, exp);
+			return n;
 		}
-		
-		return n;
 	}
 	public double evaluate(double c)
 	{
 		double total =0;
 		for(int i=0; i<a.length;i++)
 		{
-			total =total + a[i]*(Math.pow(c, i));
+			total =total + a[i]*(Math.pow(c, ex[i]));
 		}
 		return total;
 	}
@@ -57,5 +215,285 @@ public class Polynomial {
 			return true;
 		}
 		return false;
+	}
+	public Polynomial multiply(Polynomial c)
+	{
+		int siz = c.a.length*a.length;
+		int q = 0;
+		int count=0;
+		double []all = new double[siz];
+		int []all_exp = new int[siz];
+		
+		for(int i=0;i<c.a.length;i++)
+		{
+			for(int j=0;j<a.length;j++)
+			{
+				all[q]=c.a[i]*a[j];
+				all_exp[q]=c.ex[i]+ex[j];
+				q++;
+			}
+		}
+		for(int i=0; i<siz;i++)
+		{
+			for(int j=i;j<siz;j++)
+			{
+				if(all_exp[i]==all_exp[j])
+				{
+					if(i!=j)
+					{
+						all[i]=all[i]+all[j];
+						all[j]=0;
+						all_exp[j]=-1;
+					}
+				}
+			}
+		}
+		for(int i=0;i<siz;i++)
+		{
+
+//			System.out.println(all[i]);
+//			System.out.println(all_exp[i]);
+
+			if(all[i]==0||all_exp[i]==-1)
+			{
+				count++;
+			}
+		}
+
+		//System.out.println(count);
+
+		int f_siz = siz-count;
+		double []fall = new double[f_siz];
+		int []fall_exp = new int[f_siz];
+		int w=0;
+		for(int i=0;i<siz;i++)
+		{
+			if(all_exp[i]!=-1&&all[i]!=0.0)
+			{
+				//System.out.println(w);
+				//w++;
+				fall[w]=all[i];
+				fall_exp[w]=all_exp[i];
+				w++;
+			}
+		}
+		Polynomial n;
+		n=new Polynomial(fall,fall_exp);
+		return n;
+	}
+	public int after(String[] a, int pol)
+	{
+//		for(int i=0;i<pol;i++)
+//		{
+//			int q=0;
+//			while(a[q].equals("-")||a[q].equals("+"))
+//		}
+		return 0;
+	}
+	public Polynomial(File c) throws FileNotFoundException
+	{
+		Scanner s = new Scanner(c);
+		String line = (s.nextLine());
+		String[]sp = line.split("");
+		String[] b = line.split("[+-]");
+		int ind=0;
+		int check=0;
+		
+		if(sp[0].equals("-"))
+		{
+			b[0]=b[0].replace("-", "");
+		}
+		//System.out.println(b[0]);
+		if(sp[0].equals("-"))
+		{
+			check++;
+		}
+		String[]d = new String[b.length-1];
+		for(int i=0;i<sp.length;i++)
+		{
+			if(sp[i].equals("+")||sp[i].equals("-"))
+			{
+				ind++;
+				if(sp[i].equals("-"))
+				{
+					b[ind]="-"+b[ind];
+				}
+			}
+		}
+		if(check==1)
+		{
+			for(int i=1;i<b.length;i++)
+			{
+				d[i-1]=b[i];
+			}
+//			
+//			for(int i=0;i<d.length;i++)
+//			{
+//				System.out.println(d[i]);
+//			}
+			a=new double[d.length];
+			ex = new int[d.length];
+			for(int i=0;i<d.length;i++)
+			{
+				String[] bi = d[i].split("x");
+				if(bi.length==2)
+				{
+					if(bi[0].equals(""))
+					{
+						a[i]=1;
+						
+						ex[i]=Integer.parseInt(bi[1]);
+					}
+					else
+					{
+						if(!bi[0].equals("-"))
+						{
+							a[i]=Double.parseDouble(bi[0]);
+						}
+						else
+						{
+							a[i]=-1.0;
+						}
+						ex[i]=Integer.parseInt(bi[1]);
+					}
+					
+				}
+				else if(bi.length==0)
+				{
+					a[i]=1.0;
+					ex[i]=1;
+				}
+				else if(bi.length==1)
+				{
+					if(bi[0].equals(d[i]))
+					{
+						a[i]=Double.parseDouble(bi[0]);
+						ex[i]=0;
+					}
+					else if(bi[0].equals("-"))
+					{
+						a[i]=-1;
+						ex[i]=1;
+					}
+					else
+					{
+						a[i]=Double.parseDouble(bi[0]);
+						ex[i]=1;
+					}
+				
+				}
+				
+			}		
+		}
+		else {
+			
+			a=new double[b.length];
+			ex = new int[b.length];
+			for(int i=0;i<b.length;i++)
+			{
+				String[] bi = b[i].split("x");
+				if(bi.length==2)
+				{
+					if(bi[0].equals(""))
+					{
+						a[i]=1;
+						
+						ex[i]=Integer.parseInt(bi[1]);
+					}
+					else
+					{
+						if(!bi[0].equals("-"))
+						{
+							a[i]=Double.parseDouble(bi[0]);
+						}
+						else
+						{
+							a[i]=-1.0;
+						}
+						ex[i]=Integer.parseInt(bi[1]);
+					}
+					
+				}
+				else if(bi.length==0)
+				{
+					a[i]=1.0;
+					ex[i]=1;
+				}
+				else if(bi.length==1)
+				{
+
+					
+					if(bi[0].equals(b[i]))
+					{
+						a[i]=Double.parseDouble(bi[0]);
+						ex[i]=0;
+					}
+					else if(bi[0].equals("-"))
+					{
+						a[i]=-1;
+						ex[i]=1;
+					}
+					else
+					{
+						a[i]=Double.parseDouble(bi[0]);
+						ex[i]=1;
+					}
+				
+				}
+				
+			}
+			
+		}
+		s.close();
+
+	}
+	public void saveToFile(String n) throws IOException
+	{
+		File out = new File(n);
+		FileWriter wr = new FileWriter (out);
+		PrintWriter pr = new PrintWriter(wr);
+		for(int i=0;i<a.length;i++)
+		{
+			if(ex[i]==0)
+			{
+				if(a[i]<0)
+				{
+					pr.print(a[i]);
+				}
+				else if(a[i]>0&&i!=0)
+				{
+					pr.print("+" + a[i]);
+				}
+				else 
+				{
+					pr.print(a[i]);
+				}
+			}
+			else
+			{
+				if(a[i]>0)
+				{
+					if(i!=0)
+					{
+						pr.print("+"+a[i]);
+						pr.print("x");
+						pr.print(ex[i]);
+					}
+					else
+					{
+						pr.print(a[i]);
+						pr.print("x");
+						pr.print(ex[i]);
+					}
+				}
+				else
+				{
+					pr.print(a[i]);
+					pr.print("x");
+					pr.print(ex[i]);
+				}
+			}
+		}
+		pr.close();
 	}
 }
